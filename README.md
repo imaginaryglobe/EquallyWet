e21865fcb941bc48a5cd816c0033c25e798718de
Jaden Fang <jadenfang9@gmail.com>
2022-01-06 17:41:22 -0500

    fixed readme a bit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # EquallyWet
+school project for math
+
+finding the circumcenter of three points 
---
+++ main.py
@@ -0,0 +1,31 @@
+"""this is a program for the extended homework, "Equally Wet"
+the main idea of this program is to find the midpoint of three points on the coordinate plane.
+finding the midpoint is finding the circumcenter of the  three x coordinates and y coordinates"""
+
+"""
+TODO:
+1. create three points, in this case they are named A, B, and C
+2. calculate the slope between each of the points (slope ab, slope ac, and slope bc)
+3. 
+
+"""
+
+
+import math 
+import random
+
+x1 = random.randint(-100, 100)
+x2 = random.randint(-100, 100)
+x3 = random.randint(-100, 100)
+y1 = random.randint(-100, 100)
+y2 = random.randint(-100, 100)
+y3 = random.randint(-100, 100)
+
+
+
+
+a = (x1, y1)
+b = (x2, y2)
+c = (x3, y3)
+
+print(a, b, c)
